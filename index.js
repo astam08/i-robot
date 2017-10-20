d4bf6b00000a26e7a9dc803b55d824fa08e22460
Dany Sanchez <daany_57@hotmail.fr>
2017-10-20 21:15:17 +0200

    Add index.js for bot login and events

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+
+const bot = new Discord.Client();
+const Google = require('./commands/google');
+const Ping = require('./commands/ping');
+const Stream = require('./commands/stream/stream');
+const Config = require('./config');
+
+bot.on('ready', () => {
+  // bot.user.setAvatar('avatar.jpg')
+  //     .then(() => console.log('Avatar mis en place avec succès.'))
+  //     .catch(console.error)
+  // bot.user.setGame('Dreaming')
+  //     .then(() => console.log('Jeu mis en place avec succès.'))
+  //     .catch(console.error)
+  // bot.user.setUsername('I-Robot');
+});
+
+bot.on('guildMemberAdd', (member) => {
+  member.createDM()
+    .then((channel) => {
+      channel.send(`Bienvenue sur le channel ${member.displayName} :sunglasses:`);
+    }).catch();
+});
+
+bot.on('message', (message) => {
+  if (message.author.id !== bot.id) {
+    Stream.parse(message);
+    Ping.parse(message);
+    Google.parse(message);
+  }
+});
+
+bot.login(Config.token());

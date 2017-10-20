a6b0b242d2cef47147a27e43538cc646c82e4dcd
Dany Sanchez <daany_57@hotmail.fr>
2017-10-20 23:35:44 +0200

    Add hello.js handling greetings messages

---
+++ commands/hello.js
@@ -0,0 +1,25 @@
+const Command = require('./command');
+
+const helloMessage = [
+  'bonjour',
+  'hello',
+  'salut',
+  'hi',
+  'slt',
+  'bjr',
+  'yo',
+  'wesh',
+];
+
+
+module.exports = class Hello extends Command {
+  static match(message) {
+    return helloMessage
+      .some(value => message.content.toUpperCase().includes(value.toUpperCase()));
+  }
+
+  static action(message) {
+    const answer = helloMessage[Math.floor(Math.random() * helloMessage.length)];
+    message.reply(answer.charAt(0).toUpperCase() + answer.slice(1));
+  }
+};

3e56300f8379a75729a5fa8bc77569cfd4db6581
Dany Sanchez <daany_57@hotmail.fr>
2017-12-11 00:18:24 +0100

    Refactored resume.js as in pause.js

--- commands/stream/resume.js
+++ commands/stream/resume.js
@@ -1,13 +1,14 @@
 module.exports = class Resume {
   static action(self) {
     const voiceChannel = self.getVoiceChannel();
-    if (!voiceChannel.connection) {
-      self.message.reply('Je ne joue rien actuellement. :rolling_eyes:');
-    } else if (voiceChannel.connection.dispatcher &&
-               voiceChannel.connection.dispatcher.paused) {
-      voiceChannel.connection.dispatcher.resume();
+    if (voiceChannel.connection && voiceChannel.connection.dispatcher) {
+      if (voiceChannel.connection.dispatcher.paused) {
+        voiceChannel.connection.dispatcher.resume();
+      } else {
+        self.message.reply('Déjà en train de jouer ! :smile:');
+      }
     } else {
-      self.message.reply('Déjà en train de jouer ! :smile:');
+      self.message.reply('Je ne joue rien actuellement. :rolling_eyes:');
     }
   }
 };

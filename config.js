87d3ef5adf4785f95bc02b45e89ad3340df356cc
astam08 <49343371+astam08@users.noreply.github.com>
2019-04-07 22:36:08 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = class Config {
+  static token() {
+    return 'NTYxNjM2NDYwODQ4MDg3MDY1.XKoYoA.KmFwumBtLXeSfqEgMcYnhoNKd6E';
+  }
+
+  static YoutubeApiKey() {
+    return 'AIzaSyDtnhiJiY9vZk-5rGwSDnGY8LyiaNfQRWs';
+  }
+};

ddf652e95b17f770fc445958a7bdf594b0665294
astam08 <49343371+astam08@users.noreply.github.com>
2019-05-01 13:45:34 +0700

    Update stop.js

--- commands/stream/stop.js
+++ commands/stream/stop.js
@@ -6,7 +6,7 @@ module.exports = class Stop {
       voiceChannel.connection.disconnect();
       voiceChannel.leave();
     } else {
-      self.message.reply('Je ne joue rien actuellement. :rolling_eyes:');
+      self.message.reply('Terimaksih Udah Putar lagu Disni');
     }
   }
 };
